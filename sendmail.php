aa75edccbe3ad3c0ecb0a72fb8809831ed22b9a0
Your Name <nayaksekhar222@gmail.com>
2025-01-05 11:55:40 +0530

    New

---
+++ sendmail.php
@@ -0,0 +1,38 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'nayaksukanti999@gmail.com'; // Your email
+        $mail->Password = 'idfk gxed calf hmlj'; // Your email password or app password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        // Set sender and recipient
+        $mail->setFrom($email, $name); // Customer's email
+        $mail->addAddress('nayaksukanti999@gmail.com', 'Chandra Sekhar Nayak'); // Your business email
+
+        // Email content
+        $mail->isHTML(true);
+        $mail->Subject = 'New Message from Contact Form';
+        $mail->Body = "<strong>Name:</strong> $name<br><strong>Email:</strong> $email<br><strong>Message:</strong> $message";
+
+        $mail->send();
+        echo 'Message has been sent successfully.';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+?>
